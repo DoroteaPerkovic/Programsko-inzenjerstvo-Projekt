933662eb1ebee142a0b99d6ed924db207d431216
LukaMateskovic1 <luka.mateskovic@fer.unizg.hr>
2025-11-12 13:37:07 +0100

    Add files via upload

--- backend/api/adapters.py
+++ backend/api/adapters.py
@@ -5,11 +5,21 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.conf import settings
 from api.models import UserProfile
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import User
+
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
     callback_url = "http://localhost:5173" 
     client_class = OAuth2Client
+
+class NoSignupSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        user_email = sociallogin.user.email
+        if User.objects.filter(email=user_email).exists():
+            return
+        raise PermissionDenied("Korisnik s ovim emailom nije odobren za prijavu!")
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
